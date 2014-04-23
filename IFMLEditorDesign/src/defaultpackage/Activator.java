d01e46f618db42810ac3db1cf91edc9a078402e4
hamzaed <hamza.eddouibi@gmail.com>
2014-04-23 11:01:05 +0200

    fix obesign file name

--- IFMLEditorDesign/src/defaultpackage/Activator.java
+++ IFMLEditorDesign/src/defaultpackage/Activator.java
@@ -35,7 +35,7 @@ public class Activator extends AbstractUIPlugin {
       super.start(context);
 	  plugin = this;
 	  viewpoints = new HashSet<Viewpoint>();
-	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/My.odesign")); 
+	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/IFML.odesign")); 
     }
 
     /*
